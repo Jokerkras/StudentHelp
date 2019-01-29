74596c971ac39bc28a0899c0792abc64ebf738b7
Jokerkras <Krasmax97@mail.ru>
2019-01-29 15:08:18 +0300

    Added login page from firebase

--- app/src/main/java/com/example/krasm/studenthelp/view/MainActivity.kt
+++ app/src/main/java/com/example/krasm/studenthelp/view/MainActivity.kt
@@ -5,18 +5,48 @@ import android.os.Bundle
 import android.util.Log
 import com.example.krasm.studenthelp.R
 import com.example.krasm.studenthelp.presenter.MainActivityPresenter
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var database: DatabaseReference
     private lateinit var presenter: MainActivityPresenter
+    private lateinit var mFirebaseAuth: FirebaseAuth
+    private lateinit var mAuthListener: FirebaseAuth.AuthStateListener
+
+    private val RC_SIGN_IN: Int = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        mFirebaseAuth = FirebaseAuth.getInstance()
         presenter = MainActivityPresenter(this)
+        mAuthListener = FirebaseAuth.AuthStateListener {
+            var user = it.currentUser
+            if(user != null) {
+
+            } else {
+                val providers = arrayListOf( AuthUI.IdpConfig.EmailBuilder().setAllowNewAccounts(false).build() )
+                startActivityForResult(
+                        AuthUI.getInstance()
+                                .createSignInIntentBuilder()
+                                .setAvailableProviders(providers)
+                                .build(),
+                        RC_SIGN_IN)
+            }
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mFirebaseAuth.removeAuthStateListener(mAuthListener)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mFirebaseAuth.addAuthStateListener(mAuthListener)
     }
 }
